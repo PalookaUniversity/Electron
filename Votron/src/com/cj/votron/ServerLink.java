eebe1e75e7189e830b534c3b0d75c643f162b491
George Vamos <gvamos@cj.com>
2014-04-17 15:58:56 -0700

    Code template for web fetch

--- Votron/src/com/cj/votron/ServerLink.java
+++ Votron/src/com/cj/votron/ServerLink.java
@@ -1,11 +1,57 @@
 package com.cj.votron;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+
 public class ServerLink {
-	
+
 	static ServerLink instance = new ServerLink();
-	public static ServerLink getInstance(){ return instance; }
-	
-	String getText(String uri){
+
+	public static ServerLink getInstance() {
+		return instance;
+	}
+
+	String getPageText(String uri) {
+		HttpClient httpClient = new DefaultHttpClient();
+		HttpContext localContext = new BasicHttpContext();
+		HttpGet httpGet = new HttpGet(uri);
+		String text = null;
+		try {
+
+			HttpResponse response = httpClient.execute(httpGet, localContext);
+			HttpEntity entity = response.getEntity();
+			text = getASCIIContentFromEntity(entity);
+
+		} catch (Exception e) {
+			return e.getLocalizedMessage();
+		}
+		return text;
+	}
+
+	String getASCIIContentFromEntity(HttpEntity entity)
+	    throws IllegalStateException, IOException {
+
+		InputStream in = entity.getContent();
+		StringBuffer out = new StringBuffer();
+		int n = 1;
+		while (n > 0) {
+			byte[] b = new byte[4096];
+			n = in.read(b);
+			if (n > 0)
+				out.append(new String(b, 0, n));
+		}
+		return out.toString();
+	}
+
+	String getText(String uri) {
 		return "uri foo ";
 	}
 }
